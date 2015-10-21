1cbc55909863ee523b805a0e010f7bd2540ea97f
Umair Ghazi <umairghazi@gmail.com>
2015-10-21 01:02:08 -0400

    Final Commit Together
    All the Commit in one go!

---
+++ data2.js
@@ -0,0 +1,232 @@
+/**
+ * Created by umairghazi on 10/3/15.
+ */
+
+
+//Final values begin
+
+
+var Mazda6 = {
+    options: null
+};
+
+var Mazda3 = {
+    options: null
+};
+
+var CX5 = {
+    options: null
+}
+
+var Elantra = {
+    options: null
+}
+
+var Accent = {
+    options: null
+}
+
+var Sonata = {
+    options: null
+}
+
+var Soul = {
+    options: null
+}
+
+var Optima = {
+    options: null
+}
+
+var Sedona = {
+    options: null
+}
+
+var Jetta = {
+    options: null
+}
+
+var Passat = {
+    options: null
+}
+
+var Civic = {
+    options: null
+}
+
+var City = {
+    options: null
+}
+
+var Accord = {
+    options: null
+}
+
+var Fiesta = {
+    options: null
+}
+
+var Focus = {
+    options: null
+}
+
+var CClass = {
+    options: null
+}
+
+var EClass = {
+    options: null
+}
+
+var A8 = {
+    options: null
+}
+
+var TT = {
+    options: null
+}
+
+
+var A3 = {
+    options: null
+}
+
+
+var SSeries = {
+    options: null
+}
+
+
+var FSeries = {
+    options: null
+}
+
+
+var TSeries = {
+    options: null
+}
+
+
+//Final values ends
+
+
+//Romance begins
+var Mazda = {
+    options: {
+        "Mazda 3": Mazda3,
+        "Mazda 6": Mazda6,
+        "Mazda CX5": CX5
+    }
+};
+
+var Hyundai = {
+    options: {
+        "Elantra": Elantra,
+        "Accent": Accent,
+        "Sonata": Sonata
+    }
+};
+
+var Kia = {
+    options: {
+        "Soul": Soul,
+        "Optima": Optima,
+        "Sedona" : Sedona
+    }
+};
+//Romance ends
+
+
+
+
+//Thriller Starts
+
+var Volkswagen = {
+    options: {
+        "Jetta": Jetta,
+        "Passat": Passat
+    }
+};
+
+var Honda = {
+    options: {
+        "Civic": Civic,
+        "City": City,
+        "Accord": Accord
+
+    }
+};
+
+var Ford = {
+    options: {
+        "Fiesta": Fiesta,
+        "Focus": Focus
+
+    }
+};
+//Thriller Ends
+
+//SciFi Starts
+var Mercedes = {
+    options: {
+        "C Class": CClass,
+        "E Class": EClass,
+    }
+};
+
+var Audi = {
+    options: {
+        "A8": A8,
+        "A3": A3,
+        "TT":TT
+    }
+};
+
+
+var BMW = {
+    options: {
+        "7 Series": SSeries,
+        "5 Series": FSeries,
+        "3 Series": TSeries
+    }
+};
+//SciFi ends
+
+
+
+var Sedan = {
+    options: {
+        "Mazda": Mazda,
+        "Hyundai": Hyundai,
+        "Kia": Kia
+
+    }
+};
+
+var Compact = {
+    options: {
+        "Volkswagen": Volkswagen,
+        "Honda": Honda,
+        "Ford": Ford
+    }
+};
+
+var Luxury = {
+    options: {
+        "Mercedes": Mercedes,
+        "Audi": Audi,
+        "BMW": BMW
+    }
+};
+
+
+
+//Start
+var root = {
+    options: {
+        "Sedan": Sedan,
+        "Compact": Compact,
+        "Luxury": Luxury
+    }
+
+};
